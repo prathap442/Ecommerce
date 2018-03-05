c8f54d163a31a89248e8224b7209959133cc5752
prathap442 <pmohansaikrishna@gmail.com>
2018-03-05 19:46:03 +0530

    in the next  commit im adding the raty rate gem for thee stars in the comments section

--- app/helpers/cart_line_items_helper.rb
+++ app/helpers/cart_line_items_helper.rb
@@ -1,2 +1,9 @@
 module CartLineItemsHelper
+   def total_calculator
+   	sum=0
+   	@cart_line_items.each do |cart_line_item|
+   		sum+=((cart_line_item.quantity)*(cart_line_item.product.price))
+   	end
+   	return sum 
+   end	
 end
